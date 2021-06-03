2235e2d3ab070dc747eeff7acc339191225d1810
KL10Movies <65135315+muhssiin@users.noreply.github.com>
2021-06-03 14:56:48 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -20,8 +20,7 @@ async def help_cb(c, m):
     # help text
     help_text = """**You need Help?? 🧐**
 
-★ Just send me the files i will store file and give you share able link
-
+★ Join Main Channel
 
 **You can use me in channel too 😉**
 
@@ -68,11 +67,11 @@ async def about_cb(c, m):
 
 👨‍💻 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫: {owner.mention(style='md')}
 
-📢 𝐂𝐡𝐚𝐧𝐧𝐞𝐥: [NS BOT UPDATES](https://t.me/Ns_bot_updates)
+📢 𝐂𝐡𝐚𝐧𝐧𝐞𝐥: [🅼🅲🆄🅱🅴🅼🅴🅳🅸🅰](https://t.me/MCubeMediaofficial)
 
-👥 𝐆𝐫𝐨𝐮𝐩: [Ns BOT SUPPORT](https://t.me/Ns_Bot_supporters)
+👥 𝐆𝐫𝐨𝐮𝐩: [GROUP](https://t.me/joinchat/F_6ts1iYxSE4YzNl)
 
-🌐𝐒𝐨𝐮𝐫𝐜𝐞 𝐂𝐨𝐝𝐞: [Press Me 🥰](https://github.com/Ns-Bots/TG-File-Store)
+🌐𝐒𝐨𝐮𝐫𝐜𝐞 𝐂𝐨𝐝𝐞: [Press Me 🥰](https://t.me/smplmcube/7)
 """
 
     # creating buttons
